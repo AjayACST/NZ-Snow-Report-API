3716c476515db3ac8fa11e55ef3d79a2925aacc6
AjayACST <ajay@acst.co.nz>
2020-10-02 09:41:38 +1300

    Removed return before import of data

--- get-data.py
+++ get-data.py
@@ -179,7 +179,7 @@ async def treblecone(session):
     for data in soup.find_all("div", {"class": "table__cell"}):
         tc_data = data.get_text().replace("\n", "").strip()
         data_tc["data"].append(tc_data)
-<<<<<<< HEAD
+
     if data_tc["data"][40] == "No trails are currently groomed.":
         try:
             import_data_tc = {
@@ -246,126 +246,35 @@ async def treblecone(session):
             "matukituki_basin_status": data_tc["data"][35],
             "motatapu_chutes_status": data_tc["data"][37],
             "summit_slopes_status": data_tc["data"][39],
-            "main_street_groomed":data_tc["data"][41],
-            "easy_rider_groomed": data_tc["data"][43],
-            "nice_n_easy_grommed": data_tc["data"][45],
-            "raffils_run_grommed": data_tc["data"][47],
+            "easy_rider_groomed": data_tc["data"][41],
+            "nice_n_easy_grommed": data_tc["data"][43],
+            "raffils_run_grommed": data_tc["data"][45],
+            "tims_table_groomed": data_tc["data"][47],
             "petes_treat_groomed": data_tc["data"][49],
             "side_saddle_groomed": data_tc["data"][51],
             "south_ridge_wide_guid": data_tc["data"][53],
-            "tims_table_groomed": data_tc["data"][55],
             "saddle_back_groomed": data_tc["data"][57],
             "high_street_groomed": data_tc["data"][59],
             "saddle_track_groomed": data_tc["data"][61],
             "road_status": data_tc["data"][63],
-            "chain_status": data_tc["data"][65],
-            "bar_status": data_tc["data"][67],
-            "grab_and_go_status": data_tc["data"][69],
-            "the_southern_bbq": data_tc["data"][71],
-            "allpress_at_altitude": data_tc["data"][73],
+            "chain_status": data_tc["data"][6],
+            "bar_status": data_tc["data"][65],
+            "grab_and_go_status": data_tc["data"][67],
+            "the_southern_bbq": data_tc["data"][69],
+            "allpress_at_altitude": data_tc["data"][71],
             }
         except IndexError:
             pass
     x = mycol.insert_one(import_data_tc)
     id_data = x.inserted_id
     return id_data
-=======
-    try:
-        if data_tc["data"][40] == "No trails are currently groomed.":
-                import_data_tc = {
-                "data": "True",
-                "data_updated": current_time,
-                "ski_field_status": data_tc["data"][1],
-                "overnight_snowfall": data_tc["data"][5],
-                "snowfall_24_hrs": data_tc["data"][7],
-                "snowfall_7_days": data_tc["data"][9],
-                "last_snowfall_data": data_tc["data"][11],
-                "last_snowfall_amount": data_tc["data"][13],
-                "home_basin_snow_depth": data_tc["data"][15],
-                "saddle_basin_depth": data_tc["data"][17],
-                "live_temp": data_tc["data"][19],
-                "overnight_temp": data_tc["data"][21],
-                "home_basin_chair_status": data_tc["data"][23],
-                "saddle_quad_chair_status": data_tc["data"][25],
-                "nice_n_easy_status": data_tc["data"][27],
-                "magic_carpet_status": data_tc["data"][29],
-                "home_basin_status": data_tc["data"][31],
-                "saddle_basin_status": data_tc["data"][33],
-                "matukituki_basin_status": data_tc["data"][35],
-                "motatapu_chutes_status": data_tc["data"][37],
-                "summit_slopes_status": data_tc["data"][39],
-                "main_street_groomed":"No data available.",
-                "easy_rider_groomed": "No data available.",
-                "nice_n_easy_grommed": "No data available.",
-                "raffils_run_grommed": "No data available.",
-                "petes_treat_groomed": "No data available.",
-                "side_saddle_groomed": "No data available.",
-                "south_ridge_wide_guid": "No data available.",
-                "tims_table_groomed": "No data available.",
-                "saddle_back_groomed": "No data available.",
-                "high_street_groomed": "No data available.",
-                "saddle_track_groomed": "No data available.",
-                "road_status": data_tc["data"][43],
-                "chain_status": data_tc["data"][45],
-                "bar_status": data_tc["data"][47],
-                "grab_and_go_status": data_tc["data"][49],
-                "the_southern_bbq": data_tc["data"][51],
-                "allpress_at_altitude": data_tc["data"][53],
-                }
-        else:
-                import_data_tc = {
-                "data": "True",
-                "data_updated": current_time,
-                "ski_field_status": data_tc["data"][1],
-                "overnight_snowfall": data_tc["data"][5],
-                "snowfall_24_hrs": data_tc["data"][7],
-                "snowfall_7_days": data_tc["data"][9],
-                "last_snowfall_data": data_tc["data"][11],
-                "last_snowfall_amount": data_tc["data"][13],
-                "home_basin_snow_depth": data_tc["data"][15],
-                "saddle_basin_depth": data_tc["data"][17],
-                "live_temp": data_tc["data"][19],
-                "overnight_temp": data_tc["data"][21],
-                "home_basin_chair_status": data_tc["data"][23],
-                "saddle_quad_chair_status": data_tc["data"][25],
-                "nice_n_easy_status": data_tc["data"][27],
-                "magic_carpet_status": data_tc["data"][29],
-                "home_basin_status": data_tc["data"][31],
-                "saddle_basin_status": data_tc["data"][33],
-                "matukituki_basin_status": data_tc["data"][35],
-                "motatapu_chutes_status": data_tc["data"][37],
-                "summit_slopes_status": data_tc["data"][39],
-                "main_street_groomed":data_tc["data"][41],
-                "easy_rider_groomed": data_tc["data"][43],
-                "nice_n_easy_grommed": data_tc["data"][45],
-                "raffils_run_grommed": data_tc["data"][47],
-                "petes_treat_groomed": data_tc["data"][49],
-                "side_saddle_groomed": data_tc["data"][51],
-                "south_ridge_wide_guid": data_tc["data"][53],
-                "tims_table_groomed": data_tc["data"][55],
-                "saddle_back_groomed": data_tc["data"][57],
-                "high_street_groomed": data_tc["data"][59],
-                "saddle_track_groomed": data_tc["data"][61],
-                "road_status": data_tc["data"][63],
-                "chain_status": data_tc["data"][65],
-                "bar_status": data_tc["data"][67],
-                "grab_and_go_status": data_tc["data"][69],
-                "the_southern_bbq": data_tc["data"][71],
-                "allpress_at_altitude": data_tc["data"][73],
-                }
-        x = mycol.insert_one(import_data_tc)
-        id_data = x.inserted_id
-        return id_data
-    except IndexError:
-        pass
->>>>>>> c12ab93f83a45b2f030f8d2567b56836302eb0fc
 
 WAIT_TIME_SECONDS = 900
 ticker = threading.Event()
 
 async def run_def():
     async with aiohttp.ClientSession() as session:
-        print((await treblecone(session)))
+        print((await cardrona(session)))
 
 
 if __name__ == "__main__":
